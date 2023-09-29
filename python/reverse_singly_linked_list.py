035c09c641779a28aa97da7f5d2105fb4a4f0cee
rkouidri <reuben-kouidri@hotmail.com>
2023-09-29 21:00:39 +0100

    still buggy

---
+++ python/reverse_singly_linked_list.py
@@ -0,0 +1,88 @@
+class Node:
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next_node(self):
+        return self.next_node
+
+    def set_next_node(self, next_node):
+        self.next_node = next_node
+
+
+class LinkedList:
+    def __init__(self, value=None):
+        self.head_node = Node(value)
+
+    def get_head_node(self):
+        return self.head_node
+
+    def insert_beginning(self, new_value):
+        new_node = Node(new_value)
+        new_node.set_next_node(self.head_node)
+        self.head_node = new_node
+
+    def insert_end(self, new_value):
+        new_node = Node(new_value)
+        end_node = self.get_end_node()
+        end_node.set_next_node(new_node)
+
+    def get_end_node(self):
+        current_node = self.head_node
+        while current_node.next_node is not None:
+            current_node = current_node.next_node
+        return current_node
+
+    def print_linked_list(self):
+        string_list = ""
+        current_node = self.get_head_node()
+        while current_node:
+            if current_node.get_value() is not None:
+                string_list += str(current_node.get_value()) + "\n"
+            current_node = current_node.get_next_node()
+        print(string_list)
+
+    def remove_node(self, value_to_remove):
+        current_node = self.get_head_node()
+        if current_node.get_value() == value_to_remove:
+            self.head_node = current_node.get_next_node()
+        else:
+            while current_node:
+                next_node = current_node.get_next_node()
+                if next_node.get_value() == value_to_remove:
+                    current_node.set_next_node(next_node.get_next_node())
+                    current_node = None
+                else:
+                    current_node = next_node
+
+
+def make_linked_list(l):
+    ll = LinkedList()
+    for val in reversed(l):
+        ll.insert_beginning(val)
+
+    return ll
+
+
+def reverse_linked_list(ll):
+    new_ll = LinkedList()
+    while ll.get_head_node() is not None:
+        new_ll.insert_beginning(ll.get_head_node().get_value())
+        ll.remove_node(ll.get_head_node().get_value())
+
+
+
+print("Original")
+demo_list = make_linked_list([4,8,15])
+demo_list.print_linked_list()
+print("Reversed")
+reverse = reverse_linked_list(demo_list)
+reverse.print_linked_list()
+print("Original Unchanged")
+demo_list.print_linked_list()
+
+
+
