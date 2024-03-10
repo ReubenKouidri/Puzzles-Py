72d0dd4d6a516c80adc9a20c62c2bf61a5c2bc7b
Reuben Kouidri <reuben-kouidri@hotmail.com>
2024-03-10 11:11:59 +0000

    cleaned longest_substr

--- leetcode/longest_substr.py
+++ leetcode/longest_substr.py
@@ -31,32 +31,20 @@ s consists of English letters, digits, symbols and spaces.
 
 
 def longest_substring(s):
-    last_seen = {}  # Step 1
+    d = dict()
     l, max_len = 0, 0
-
     for r, char in enumerate(s):
-        if char in last_seen and last_seen[
-            char] >= l:  # If char was seen in the current window
-            l = last_seen[char] + 1  # Move the start of the window
-        last_seen[char] = r  # Update the last seen position
-        max_len = max(max_len,
-                      r - l + 1)  # Update max_len if current window is larger
+        if d.get(char) is None:
+            d[char] = 1
+        else:
+            d[char] += 1
+            while d[char] > 1 and l <= r:
+                d[s[l]] -= 1
+                l += 1
+
+        max_len = max(max_len, r - l + 1)
 
     return max_len
-    # d = dict()
-    # l, max_len = 0, 0
-    # for r, char in enumerate(s):
-    #     if d.get(char) is None:
-    #         d[char] = 1
-    #     else:
-    #         d[char] += 1
-    #         while d[char] > 1 and l <= r:
-    #             d[s[l]] -= 1
-    #             l += 1
-    #
-    #     max_len = max(max_len, r - l + 1)
-    #
-    # return max_len
 
 
 def test():
